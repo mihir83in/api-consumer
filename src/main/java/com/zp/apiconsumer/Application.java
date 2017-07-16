aeaea16fa15631291171ad12806bde05ae7e9249
mihir <mihir83in@yahoo.com>
2017-07-17 01:49:43 +0530

    trying maven with docker

--- src/main/java/com/zp/apiconsumer/Application.java
+++ src/main/java/com/zp/apiconsumer/Application.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.retry.annotation.EnableRetry;
 
-@ComponentScan(excludeFilters = @ComponentScan.Filter(value =
+@ComponentScan(lazyInit = true, excludeFilters = @ComponentScan.Filter(value =
         {CurrencyLayerClientConfiguration.class, OxrClientConfiguration.class}, type = FilterType.ASSIGNABLE_TYPE))
 @EnableFeignClients
 @EnableRetry
